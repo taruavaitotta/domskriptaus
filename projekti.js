8d875a39b2dc4db6f1998151c3c8c1f6d83052cd
taruavaitotta <112821656+taruavaitotta@users.noreply.github.com>
2023-10-11 16:08:35 +0300

    Update projekti.js
    listaan voi lisätä asioita

--- projekti.js
+++ projekti.js
@@ -6,6 +6,48 @@
     <title>Document</title>
 </head>
 <body>
+  <div id="myDIV" class="header">
+    <h2>To Do List</h2>
+    <input type="text" id="myInput" placeholder="Title...">
+    <span onclick="newElement()" class="addBtn">Lisää listaan</span>
+  </div>
+  
+  <ul id="myUL">
+    <li>aavikkokettu</li>
+    <li>napakettu</li>
+    <li>ketunleipä</li>
+    <li>kettu repolainen</li>
+    <li>ketuttaa</li>
+  </ul> 
     
+
+<script>
+    
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Alahan naputtaa!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+} 
+</script>
+   
 </body>
 </html>
