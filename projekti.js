cf8e39f66bbdfb77f384408f1dbf9c43c43a8b25
taruavaitotta <112821656+taruavaitotta@users.noreply.github.com>
2023-10-16 15:07:50 +0300

    Update projekti.js

--- projekti.js
+++ projekti.js
@@ -1,68 +1,114 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>To-Do List</title>
+  <style>
+    body {
+      font-family: Verdana, Tahoma, sans-serif, Arial, sans-serif;
+      background-color: #C8E4B2;
+      margin: 0;
+      padding: 0;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+
+    .header {
+      background-color: #9ED2BE; 
+      padding: 10px;
+      color: #005599;
+      text-align: center;
+    }
+
+     #myInput {
+      width: 70%;
+      padding: 10px;
+      border: none;
+      margin: 10px;
+    }
+    #myDIV {
+      width: 20%;
+      height: 70vh;
+      padding: 10px;
+      border: none;
+      margin: 10px;
+    }
+
+
+    .addBtn button {
+      background-color: #0077B6; 
+      color: white;
+      border: none;
+      padding: 10px 20px;
+      cursor: pointer;
+    }
+
+    .addBtn button:hover {
+      background-color: #005599; 
+    }
+
+    .ul {
+      list-style: none;
+      padding: 0;
+    }
+
+    li {
+      display: flex;
+      align-items: center;
+      padding: 10px;
+      background-color: white;
+      margin: 5px 0;
+      box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
+    }
+
+    .checkbox {
+      width: 20px;
+      height: 20px;
+      margin-right: 10px;
+      border: 2px solid #0077B6;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .check-mark {
+      font-weight: bold;
+      font-size: 18px;
+    }
+
+    .task-done {
+      background-color: #EFEFEF;
+    }
+
+    .delete-button {
+      background-color: #0077B6; 
+      color: white;
+      border: none;
+      padding: 10px 20px;
+      cursor: pointer;
+    }
+
+    .delete-button:hover {
+      background-color: #005599; 
+    }
+  </style>
 </head>
-<style>
-   
-  .header {
-    background-color: #C8E4B2;
-    padding: 20px;
-    color: rgb(6, 1, 1);
-    text-align: center;
-  }
-
-   .ul {
-    list-style: none;
-    padding: 0;
-    background-color: #9ED2BE;
-    
-  }
-
-  li {
-    display: flex;
-    align-items: center;
-  }
-
-  .checkbox {
-    width: 20px;
-    height: 20px;
-    margin-right: 10px;
-    border: 1px solid #000;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
-
-  .check-mark {
-    font-weight: bold;
-  }   
-  
-
- 
-</style>
-
 <body>
   <div id="myDIV" class="header">
     <h2>To Do List</h2>
     <input type="text" id="myInput" placeholder="Title...">
     <span onclick="newElement()" class="addBtn"><button>Lisää listaan</button></span>
-  </div>
+  
 
   <div id="myUL" class="ul">
-    <p><li></li></p>
-  
   </div>
-  
-    
-  
-
+</div>
   <button class="delete-button" onclick="deleteCompletedTasks()">Poista tehdyt tehtävät</button>
-  
 
   <script>
     function newElement() {
@@ -99,6 +145,7 @@
         checkMark.textContent = "✔";
       }
     }
+
     function deleteCompletedTasks() {
       var completedTasks = document.querySelectorAll(".task-done");
       completedTasks.forEach(function(task) {
